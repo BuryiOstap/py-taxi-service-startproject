02487ff80dddcfd5916714872abfbbd6e14c05ff
Ostap <ostap.burij@gmail.com>
2025-11-03 10:52:09 +0200

    added str metod

--- taxi/models.py
+++ taxi/models.py
@@ -10,12 +10,21 @@ class Manufacturer(models.Model):
     name = models.CharField(max_length=120, unique=True)
     country = models.CharField(max_length=120)
 
+    def __str__(self):
+        return f"{self.name} ({self.country})"
+
 
 class Driver(AbstractUser):
     license_number = models.CharField(max_length=120, unique=True)
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} {self.license_number}"
+
 
 class Car(models.Model):
     model = models.CharField(max_length=120)
-    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
-    drivers = models.ManyToManyField(settings.AUTH_USER_MODEL)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, related_name="cars")
+    drivers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="cars")
+
+    def __str__(self):
+        return f"{self.manufacturer} {self.model}"
